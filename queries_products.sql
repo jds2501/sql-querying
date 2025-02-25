a42fcc07a3c8b5013ec6298d3279f3b4097d5f61
Jason Smith <jasondsmith2501@gmail.com>
2025-02-25 10:45:18 -0800

    Finished products querying exercise

--- queries_products.sql
+++ queries_products.sql
@@ -11,4 +11,34 @@ INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, true
 INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, false);
 
 -- 4. Display all rows and columns in the table
-SELECT * from products;
+SELECT * FROM products;
+
+-- 5. Display product names only
+SELECT name FROM products;
+
+-- 6. Display product name and price
+SELECT name, price FROM products;
+
+-- 7. Add a product of my choice
+INSERT INTO products (name, price, can_be_returned) VALUES ('Nintendo Switch', 250.00, true);
+
+-- 8. Display products that can be returned
+SELECT * FROM products where can_be_returned = true;
+
+-- 9. Display price < 44
+SELECT * FROM products WHERE price < 44.00;
+
+-- 10. Price between 22.50 & 99.99
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+
+-- 11. Sale - 20 dollars off, update
+UPDATE products SET price = price - 20.0;
+
+-- 12. < 25 dollars is sold out
+DELETE FROM products WHERE price < 25;
+
+-- 13. Increase remaining products by 20 dollars
+UPDATE products SET price = price + 20.0;
+
+-- 14. Everything is returnable
+UPDATE products Set can_be_returned = true;
