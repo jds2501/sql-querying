ed01dc86593f8e8362f4e9be94b4307c673dcf3a
Jason Smith <jasondsmith2501@gmail.com>
2025-02-25 21:37:50 -0800

    Fixed based on solution feedback

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -8,7 +8,7 @@ SELECT * FROM analytics WHERE id = 1880;
 SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
 
 -- 3. Count number of apps per category
-SELECT category, COUNT(category) FROM analytics GROUP BY category;
+SELECT category, COUNT(*) FROM analytics GROUP BY category;
 
 -- 4. Top 5 most reviewed apps
 SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
@@ -41,13 +41,19 @@ SELECT app_name FROM analytics ORDER BY price DESC LIMIT 1;
 SELECT SUM(reviews) FROM analytics;
 
 -- 14. Find all categories with more than 300 apps
-SELECT category FROM analytics GROUP BY category HAVING COUNT(app_name) > 300;
+SELECT category FROM analytics GROUP BY category HAVING COUNT(*) > 300;
 
 -- 15. App with highest proportion of min_install to reviews, with apps taht have been installed at least 100000 times
 SELECT app_name, reviews, min_installs, min_installs / reviews AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY min_installs / reviews DESC LIMIT 1;
 
 -- FS1. Name & rating of top rated apps in each category (apps installed at least 50000 times)
-SELECT app_name, rating FROM analytics a WHERE min_installs >= 50000 AND rating = (SELECT MAX(rating) FROM analytics WHERE category = a.category);
+SELECT app_name, rating, category FROM analytics
+  WHERE (rating, category) in (
+    SELECT MAX(rating), category FROM analytics
+      WHERE min_installs >= 50000
+      GROUP BY category
+    )
+  ORDER BY category;
 
 -- FS2. Find all the apps that have a name similar to “facebook”.
 SELECT app_name FROM analytics WHERE app_name ILIKE '%facebook%';
