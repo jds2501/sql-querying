366734d8e88d4f94aeaf8fcac16c2cc533c6e58b
Jason Smith <jasondsmith2501@gmail.com>
2025-02-28 11:29:17 -0800

    Added one join operation

--- euro2012_queries.sql
+++ euro2012_queries.sql
@@ -3,3 +3,10 @@
 
 -- Show match ID & player for players from Russia
 SELECT matchid, player FROM goal WHERE teamid = 'GER';
+
+-- Show id, stadium, team1, team2  for game 1012
+SELECT id, stadium, team1, team2 FROM game WHERE id = 1012;
+
+-- Show player, teamid, stadium, and mdate for every German goal
+SELECT goal.player, goal.teamid, game.stadium, game.mdate FROM game INNER JOIN goal ON (game.id = goal.matchid) WHERE goal.teamid = 'GER';
+
