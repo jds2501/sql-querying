d9df0a244c231a5629cf99bc395635440797d6f3
Jason Smith <jasondsmith2501@gmail.com>
2025-03-01 22:42:55 -0800

    Finished euro 2012 JOIN exercises

--- euro2012_queries.sql
+++ euro2012_queries.sql
@@ -24,3 +24,21 @@ SELECT goal.player FROM goal INNER JOIN game ON (goal.matchid = game.id) WHERE s
 
 --- Show players who scored a goal against Germany
 SELECT DISTINCT goal.player FROM goal INNER JOIN game ON (goal.matchid = game.id) WHERE goal.teamid != 'GER' AND (game.team1 = 'GER' OR game.team2 = 'GER');
+
+--- Total goals by team name
+SELECT eteam.teamname, COUNT(goal.teamid) AS goals FROM eteam INNER JOIN goal ON (goal.teamid = eteam.id) GROUP BY eteam.teamname;
+
+--- Total goals scored by stadium
+SELECT game.stadium, COUNT(goal.matchid) AS goals FROM game INNER JOIN goal ON (goal.matchid = game.id) GROUP BY game.stadium;
+
+-- Show matchid, date, num of goals scored when Poland was playing
+SELECT game.id, game.mdate, COUNT(goal.matchid) AS goals FROM game INNER JOIN goal ON (goal.matchid = game.id) WHERE game.team1 = 'POL' OR game.team2 = 'POL' GROUP BY game.id;
+
+-- Show matchid, date, and number of goals in games where GER scored
+SELECT game.id, game.mdate, COUNT(goal.matchid) AS goals FROM game INNER JOIN goal ON (goal.matchid = game.id) WHERE goal.teamid = 'GER' GROUP BY game.id;
+
+-- Every match showing overall team scores
+SELECT game.mdate, game.team1, SUM(CASE WHEN goal.teamid = game.team1 THEN 1 ELSE 0 END) AS score1, 
+    game.team2, SUM(CASE WHEN goal.teamid = game.team2 THEN 1 ELSE 0 END) AS score2
+    FROM game INNER JOIN goal ON (goal.matchid = game.id) GROUP BY game.mdate, game.team1, game.team2
+    ORDER BY game.mdate, game.team1, game.team2;
