c149d2502e3daae4ee106412f931495969c6507e
Jason Smith <jasondsmith2501@gmail.com>
2025-03-02 19:13:35 -0800

    Finished hard questions for movie queries

--- movie_queries.sql
+++ movie_queries.sql
@@ -37,3 +37,30 @@ SELECT movie.title FROM actor JOIN casting ON (actor.id = casting.actorid)
 SELECT movie.title, actor.name FROM actor JOIN casting ON (actor.id = casting.actorid)
     JOIN movie ON (casting.movieid = movie.id) WHERE movie.yr = 1962
     AND casting.ord = 1;
+
+-- Movie count by year > 2 movies by Rock Hudson
+SELECT movie.yr, COUNT(movie.yr) FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE actor.name = 'Rock Hudson'
+    GROUP BY movie.yr HAVING COUNT(movie.yr) > 2;
+
+-- Film title and leading actors where Julie Andrews was in the movie
+SELECT movie.title, actor.name FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE casting.ord = 1 AND
+    movie.id IN (SELECT movie.id FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE actor.name = 'Julie Andrews');
+
+-- Actors with at least 15 starring roles
+SELECT actor.name FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE casting.ord = 1
+    GROUP BY actor.id HAVING COUNT(casting.movieid) >= 15 ORDER BY actor.name;
+
+-- Films in 1978 ordered by number of actors in cast, then title
+SELECT movie.title FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE movie.yr = 1978 
+    GROUP BY movie.id ORDER BY COUNT(casting.actorid) DESC, movie.title;
+
+-- All people who have worked with Art Garfunkel
+SELECT actor.name FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE actor.name != 'Art Garfunkel'
+    AND movie.id IN (SELECT movie.id FROM actor JOIN casting ON (actor.id = casting.actorid)
+    JOIN movie ON (casting.movieid = movie.id) WHERE actor.name = 'Art Garfunkel');
